a1591248bf5450c6091176c260ada58449853241
mauricio-rodriguez <42778143+mauricio-rodriguez@users.noreply.github.com>
2019-12-02 01:42:04 -0500

    colores con threads

---
+++ Headers.h
@@ -0,0 +1,16 @@
+//
+// Created by mauri on 01-Dec-19.
+//
+
+#ifndef PC4_HEADERS_H
+#define PC4_HEADERS_H
+#include <iostream>
+#include <vector>
+#include <string_view>
+#include "lodepng.h"
+#include <thread>
+#include <cmath>
+using namespace std;
+auto nt = thread::hardware_concurrency();
+
+#endif //PC4_HEADERS_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-﻿#include <iostream>
-#include <vector>
-#include <string_view>
-#include "lodepng.h"
+﻿
+
+#include "Headers.h"
+
 using namespace std;
 
 std::vector<unsigned char> decode(string_view filename,
@@ -21,14 +21,96 @@ void encode(string_view filename, std::vector<unsigned char>& image, unsigned wi
         std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
     }
 }
+void green_filter(vector<unsigned char> imagen,int w,int h) {
+    auto temp = imagen;
+    auto colored=[&temp](int h, int m, int w){
+        for (size_t i = h; i < m; i++) {
+            for (size_t j = 0; j < w * 4; j += 4) {
+                temp[i * w * 4 + j + 0] = 0;
+                temp[i * w * 4 + j + 2] = 0;
+            }
+        }
+    };
 
+    auto end=(h/nt);
+    auto initial=0;
+    vector<thread> vt(nt);
+    for (int i=0;i<vt.size();i++){
+        vt[i]=thread (colored, initial, end, w);
+        initial+=h/nt;
+        end+=h/nt;
+    }
+    for (auto &t: vt)
+        t.join();
+    encode("green.png",temp,w,h);
+}
+void red_filter(vector<unsigned char> imagen,int w,int h) {
+    auto temp = imagen;
+    auto colored=[&temp](int h, int m, int w){
+        for (size_t i = h; i < m; i++) {
+            for (size_t j = 0; j < w * 4; j += 4) {
+                temp[i * w * 4 + j + 1] = 0;
+                temp[i * w * 4 + j + 2] = 0;
+            }
+        }
+    };
+    auto end=(h/nt);
+    auto initial=0;
+    vector<thread> vt(nt);
+    for (int i=0;i<vt.size();i++){
+        vt[i]=thread (colored, initial, end, w);
+        initial+=h/nt;
+        end+=h/nt;
+    }
+    for (auto &t: vt)
+        t.join();
+    encode("red.png",temp,w,h);
+}
+void blue_filter(vector<unsigned char> imagen,int w,int h) {
+    auto temp = imagen;
+    auto colored=[&temp](int h, int m, int w){
+        for (size_t i = h; i < m; i++) {
+            for (size_t j = 0; j < w * 4; j += 4) {
+                temp[i * w * 4 + j + 0] = 0;
+                temp[i * w * 4 + j + 1] = 0;
+            }
+        }
+    };
+    auto end=(h/nt);
+    auto initial=0;
+    vector<thread> vt(nt);
+    for (int i=0;i<vt.size();i++){
+        vt[i]=thread (colored, initial, end, w);
+        initial+=h/nt;
+        end+=h/nt;
+    }
+    for (auto &t: vt)
+        t.join();
+    encode("blue.png",temp,w,h);
+}
+void rotate(vector<unsigned char> imagen,int w,int h) {
+    auto temp = imagen;
+    for (size_t i = 0; i < h; i++) {
+        for (size_t j = 0; j < w * 4; j += 4) {
+            temp[(i*w*4+j+0)*sin(45)]=temp[i * w * 4 + j + 0]; // Red component
+            temp[(i*w*4+j+1)*sin(45)]=temp[i * w * 4 + j + 1];
+            temp[(i*w*4+j+2)*sin(45)]=temp[i * w * 4 + j + 2];
+            temp[(i*w*4+j+3)*sin(45)]=temp[i * w * 4 + j + 3];
+        }
+    }
+    encode("rotated.png",temp,w,h);
+}
 int main()
 {
     unsigned int w;
     unsigned int h;
-    auto image = decode("../in.png", w, h);
+    auto image = decode("in.png", w, h);
+    //green_filter(image,w,h);
+    green_filter(image,w,h);
+    //blue_filter(image,w,h);
+    //rotate(image,w,h);
     // Un comment if you want to check buffer content
-    for (size_t i = 0; i < h; i++) {
+    /*for (size_t i = 0; i < h; i++) {
         for (size_t j = 0; j < w * 4; j += 4) {
             int r = image[i * w * 4 + j + 0]; // Red component
             int g = image[i * w * 4 + j + 1]; // Green component
@@ -41,6 +123,10 @@ int main()
         }
         std::cout << endl;
     }
-    encode("../out.png", image, w, h);
+*/
+    auto nt = thread::hardware_concurrency();
+    auto range =  image.size()/ nt;
+
     return 0;
+
 }
